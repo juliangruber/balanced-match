19bd0bb051b595fcd865783ddbc0a4f3636307cc
jerome@work <github@jeromedecoster.com>
2014-04-24 12:30:59 +0200

    a and b can be longer than 1 char

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 module.exports = function(a, b, str) {
   var bal = 0;
   var m = {};
+  var al = a.length;
+  var bl = b.length;
 
-  for (var i = 0; i < str.length; i++) {
-    if (str[i] == a) {
+  for (var i = 0, n = str.length; i < n; i++) {
+    if (a == str.substr(i, al)) {
       if (!('start' in m)) m.start = i;
       bal++;
     }
-    else if (str[i] == b && 'start' in m) {
+    else if (b == str.substr(i, bl) && 'start' in m) {
       bal--;
       if (!bal) {
         m.end = i;
         m.pre = str.substr(0, m.start);
         m.body = (m.end - m.start > 1)
-          ? str.substring(m.start + 1, m.end)
+          ? str.substring(m.start + al, m.end)
           : '';
-        m.post = str.slice(m.end + 1);
+        m.post = str.slice(m.end + bl);
         return m;
       }
     }
--- test/balanced.js
+++ test/balanced.js
@@ -24,5 +24,19 @@ test('balanced', function(t) {
     post: 'between{body2}post'
   });
   t.notOk(balanced('{', '}', 'nope'), 'should be notOk');
+  t.deepEqual(balanced('<b>', '</b>', 'pre<b>in<b>nest</b></b>post'), {
+    start: 3,
+    end: 19,
+    pre: 'pre',
+    body: 'in<b>nest</b>',
+    post: 'post'
+  });
+  t.deepEqual(balanced('<b>', '</b>', 'pre</b><b>in<b>nest</b></b>post'), {
+    start: 7,
+    end: 23,
+    pre: 'pre</b>',
+    body: 'in<b>nest</b>',
+    post: 'post'
+  });
   t.end();
 });
