58135338f86d60377dc54fd9964476fc1352f3cd
Julian Gruber <julian@juliangruber.com>
2014-04-24 05:43:31 -0700

    refactor

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 module.exports = function(a, b, str) {
   var bal = 0;
   var m = {};
-  var al = a.length;
-  var bl = b.length;
 
-  for (var i = 0, n = str.length; i < n; i++) {
-    if (a == str.substr(i, al)) {
+  for (var i = 0; i < str.length; i++) {
+    if (a == str.substr(i, a.length)) {
       if (!('start' in m)) m.start = i;
       bal++;
     }
-    else if (b == str.substr(i, bl) && 'start' in m) {
+    else if (b == str.substr(i, b.length) && 'start' in m) {
       bal--;
       if (!bal) {
         m.end = i;
         m.pre = str.substr(0, m.start);
         m.body = (m.end - m.start > 1)
-          ? str.substring(m.start + al, m.end)
+          ? str.substring(m.start + a.length, m.end)
           : '';
-        m.post = str.slice(m.end + bl);
+        m.post = str.slice(m.end + b.length);
         return m;
       }
     }
