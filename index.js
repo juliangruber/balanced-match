4052418a7bba75427605845d82b0a3e87aa86ba3
Dustin Dowell <dustindowell22@gmail.com>
2015-09-01 17:32:49 -0500

    Add var and semicolons

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = balanced;
 function balanced(a, b, str) {
-  matchA = str.match(a)
-  matchB = str.match(b)
+  var matchA = str.match(a);
+  var matchB = str.match(b);
 
   a = a instanceof RegExp && matchA ? matchA[0] : a;
   b = b instanceof RegExp && matchB ? matchB[0] : b;
